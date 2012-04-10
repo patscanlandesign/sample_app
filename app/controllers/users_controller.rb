65f5def4d11d09c33f4ff0bfe1ef38d5ac9993df
Pat Scanlan <patscanlandesign@yahoo.com>
2012-04-10 13:28:58 -0700

    Finished with 10.3. Resume with 10.4 - Destroying users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	before_filter :authenticate, :only => [:index, :edit, :update]
+	before_filter :correct_user, :only => [:edit, :update]
+	
+	def index
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
 	
 	def show
 		@user = User.find(params[:id])
@@ -25,12 +32,10 @@ class UsersController < ApplicationController
 	end
 	
 	def edit
-		@user = User.find(params[:id])
 		@title = "Edit user"	
 	end
 	
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(params[:user])
 			flash[:success] = "Profile updated."
 			redirect_to @user
@@ -40,4 +45,14 @@ class UsersController < ApplicationController
 		end
 	end
 	
+	private
+	
+		def authenticate
+			deny_access unless signed_in?
+		end
+		
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
 end
