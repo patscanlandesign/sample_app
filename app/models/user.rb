5b1f06eee2289ffc356cfa1c1f461694922c9e13
Pat Scanlan <patscanlandesign@yahoo.com>
2012-06-28 17:26:47 -0700

    finished with 12.1 - continue with 12.2 A web interface for following and followers. Work version.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	
 	has_many :microposts, :dependent => :destroy
+	has_many :relationships,:foreign_key => "follower_id",
+												  :dependent => :destroy
+	has_many :following, :through => :relationships, :source => :followed
+	
+	has_many :reverse_relationships, :foreign_key => "followed_id",
+															     :class_name => "Relationship",
+																	 :dependent => :destroy
+	has_many :followers, :through => :reverse_relationships, :source => :follower
 	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
@@ -40,6 +48,18 @@ class User < ActiveRecord::Base
 	def feed
 		#This is preliminary. See Chapter 11 for the full implementation.
 		Micropost.where("user_id =?", id)
+	end
+	
+	def following?(followed)
+		relationships.find_by_followed_id(followed)
+	end
+	
+	def follow!(followed)
+		relationships.create!(:followed_id => followed.id)
+	end
+	
+	def unfollow!(followed)
+		relationships.find_by_followed_id(followed).destroy
 	end
 	
 	private
