637997058f05f5b06b3fb0cadc6d7a3463125494
Pat Scanlan <patscanlandesign@yahoo.com>
2012-07-10 10:31:27 -0700

    committing changes from a few days ago

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,55 +7,81 @@ describe PagesController do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-    
-	it "should have the right title" do
-	  get 'home'
-	  response.should have_selector("title",
-                                    :content => @base_title + " | Home")
-	end
-  end	
-
   describe "GET 'contact'" do
-    it "should be successful" do
+    
+		it "should be successful" do
       get 'contact'
       response.should be_success
     end
 	
-	it "should have the right title" do
-	get 'contact'
-	response.should have_selector("title",
-                                  :content => @base_title + " | Contact")
+		it "should have the right title" do
+			get 'contact'
+			response.should have_selector("title",
+																		:content => "#{@base_title} | Contact")
+		end	
 	end
-  end	
   
   describe "GET 'about'" do
-    it "should be successful" do
-	  get 'about'
-	  response.should be_success
-	end
+    
+		it "should be successful" do
+			get 'about'
+			response.should be_success
+		end
 	
-	it "should have the right title" do
-	get 'about'
-	response.should have_selector("title",
-                                  :content => @base_title + " | About")
-	end
+		it "should have the right title" do
+			get 'about'
+			response.should have_selector("title",
+																		:content => "#{@base_title} | About")
+		end
   end
 
   describe "GET 'help'" do
-	it "should be succesful" do
-	  get 'help'
-	  response.should be_success
-	end
+		
+		it "should be succesful" do
+			get 'help'
+			response.should be_success
+		end
 	
-	it "should have the right title" do
-	get 'help'
-	response.should have_selector("title",
-                                  :content => @base_title + " | Help")
+		it "should have the right title" do
+			get 'help'
+			response.should have_selector("title",
+																		:content => "#{@base_title} | Help")
+		end
 	end
-  end	
+  
+	describe "GET 'home'" do
+	
+		describe "when not signed in" do
+		
+			before(:each) do
+				get :home
+			end
+			
+			it "should be successful" do
+				response.should be_success
+			end
+			
+			it "should have the right title" do
+				response.should have_selector("title",
+																			:content => "#{@base_title} | Home")
+			end
+		end
+		
+		describe "when signed in" do
+		
+			before(:each) do
+				@user = test_sign_in(Factory(:user))
+				other_user = Factory(:user, :email => Factory.next(:email))
+				other_user.follow!(@user)
+			end
+			
+			it "should have the right follower/following counts" do
+				get :home
+				response.should have_selector("a", :href => following_user_path(@user),
+																					 :content => "0 following")
+				response.should have_selector("a", :href => followers_user_path(@user),
+																					 :content => "1 follower")
+			end
+		end
+	end		
 end
